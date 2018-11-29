dfb7428c739df7efda11e5a9010e76487361dce7
Erik Solveson <erik.solveson@wsu.edu>
2018-11-29 10:57:07 -0800

    Add files via upload

---
+++ odd_even_checker.py
@@ -0,0 +1,19 @@
+num = input("Please enter a number: ")
+check = input("2nd number please: ")
+
+if num % 4 == 0:
+    print("your number is a multiple of 4")
+elif num % 2 == 1:
+    print ("Your number is odd")
+else:
+    print("your number is even")
+
+
+#num = input("Please enter a number: ")
+#check = input("2nd number please: ")
+#
+#if num % check == 0:
+#    print ("Your first number divides evenly into your second")
+#else:
+#    print("Your numbers do not divide evenly")
+
